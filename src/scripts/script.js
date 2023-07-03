4f12a09ea7da1489701a72466fab2f3327723aca
Daniil <detromixxan@gmail.com>
2023-07-03 20:48:13 +0300

    Add validation form

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,3 +7,25 @@ function myFunction() {
 function myFunction2() {
     targetDiv.style.display = "none";
 }
+//валидац функция
+function validateForm() {
+	var nameInput = document.getElementById("name");
+	var emailInput = document.getElementById("email");
+	var nameError = document.getElementById("nameError");
+	var emailError = document.getElementById("emailError");
+
+	nameError.textContent = ""; // Очищаем сообщение об ошибке перед проверкой
+	emailError.textContent = "";
+
+	if (nameInput.value.trim() === "") {
+		nameError.textContent = "Пожалуйста, введите ваше имя.";
+		return;
+	}
+
+	if (emailInput.value.trim() === "") {
+		emailError.textContent = "Пожалуйста, введите вашу почту.";
+		return;
+	}
+
+	alert("До связи :)");
+}
