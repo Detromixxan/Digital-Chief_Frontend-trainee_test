cff5e68f5d2e38a64d942c8aa4b1fbc2c6abcb65
Daniil <detromixxan@gmail.com>
2023-07-03 19:59:47 +0300

    Add burger-menu

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+let targetDiv= document.getElementById("burger-menu")
+
+//функция закрытия 
+function myFunction() {
+    targetDiv.style.display = "block";
+}
+function myFunction2() {
+    targetDiv.style.display = "none";
+}
